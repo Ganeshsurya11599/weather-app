062ad6f307213c07d78c53e5423f5ea762e86b9a
Ganeshsurya11599 <a.surya11599@gnail.com>
2019-12-14 00:22:42 +0530

    Weather-app

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ApixuService } from '../apixu.service';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent implements OnInit {
+  public weatherSearchForm: FormGroup;
+  public weatherData: any;
+  date=new Date();
+  constructor( private formbuilder : FormBuilder,
+                private apixuservice:ApixuService
+  ) { }
+
+  ngOnInit() {
+    this.weatherSearchForm = this.formbuilder.group({
+      location:['']
+    });
+  }
+
+  sendToAPIXU(formValues) {
+    this.apixuservice.getWeather(formValues.location).subscribe(data => {
+      this.weatherData = data;
+      console.log(this.weatherData);
+    });    
+    
+}
+
+}
